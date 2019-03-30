829e26831644b5be0918a3edff9ffffd82cf36c6
Sean Chang <seanarwa@gmail.com>
2019-03-30 14:58:30 -0700

    Amplify API - graphQL schema defined

---
+++ src/graphql/mutations.js
@@ -0,0 +1,117 @@
+// eslint-disable
+// this is an auto generated file. This will be overwritten
+
+export const createUser = `mutation CreateUser($input: CreateUserInput!) {
+  createUser(input: $input) {
+    id
+    username
+    email
+    emailVerified
+    notes {
+      items {
+        id
+        createdAt
+        longitude
+        latitude
+        content
+      }
+      nextToken
+    }
+  }
+}
+`;
+export const updateUser = `mutation UpdateUser($input: UpdateUserInput!) {
+  updateUser(input: $input) {
+    id
+    username
+    email
+    emailVerified
+    notes {
+      items {
+        id
+        createdAt
+        longitude
+        latitude
+        content
+      }
+      nextToken
+    }
+  }
+}
+`;
+export const deleteUser = `mutation DeleteUser($input: DeleteUserInput!) {
+  deleteUser(input: $input) {
+    id
+    username
+    email
+    emailVerified
+    notes {
+      items {
+        id
+        createdAt
+        longitude
+        latitude
+        content
+      }
+      nextToken
+    }
+  }
+}
+`;
+export const createNote = `mutation CreateNote($input: CreateNoteInput!) {
+  createNote(input: $input) {
+    id
+    createdAt
+    longitude
+    latitude
+    content
+    user {
+      id
+      username
+      email
+      emailVerified
+      notes {
+        nextToken
+      }
+    }
+  }
+}
+`;
+export const updateNote = `mutation UpdateNote($input: UpdateNoteInput!) {
+  updateNote(input: $input) {
+    id
+    createdAt
+    longitude
+    latitude
+    content
+    user {
+      id
+      username
+      email
+      emailVerified
+      notes {
+        nextToken
+      }
+    }
+  }
+}
+`;
+export const deleteNote = `mutation DeleteNote($input: DeleteNoteInput!) {
+  deleteNote(input: $input) {
+    id
+    createdAt
+    longitude
+    latitude
+    content
+    user {
+      id
+      username
+      email
+      emailVerified
+      notes {
+        nextToken
+      }
+    }
+  }
+}
+`;
---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,117 @@
+// eslint-disable
+// this is an auto generated file. This will be overwritten
+
+export const onCreateUser = `subscription OnCreateUser {
+  onCreateUser {
+    id
+    username
+    email
+    emailVerified
+    notes {
+      items {
+        id
+        createdAt
+        longitude
+        latitude
+        content
+      }
+      nextToken
+    }
+  }
+}
+`;
+export const onUpdateUser = `subscription OnUpdateUser {
+  onUpdateUser {
+    id
+    username
+    email
+    emailVerified
+    notes {
+      items {
+        id
+        createdAt
+        longitude
+        latitude
+        content
+      }
+      nextToken
+    }
+  }
+}
+`;
+export const onDeleteUser = `subscription OnDeleteUser {
+  onDeleteUser {
+    id
+    username
+    email
+    emailVerified
+    notes {
+      items {
+        id
+        createdAt
+        longitude
+        latitude
+        content
+      }
+      nextToken
+    }
+  }
+}
+`;
+export const onCreateNote = `subscription OnCreateNote {
+  onCreateNote {
+    id
+    createdAt
+    longitude
+    latitude
+    content
+    user {
+      id
+      username
+      email
+      emailVerified
+      notes {
+        nextToken
+      }
+    }
+  }
+}
+`;
+export const onUpdateNote = `subscription OnUpdateNote {
+  onUpdateNote {
+    id
+    createdAt
+    longitude
+    latitude
+    content
+    user {
+      id
+      username
+      email
+      emailVerified
+      notes {
+        nextToken
+      }
+    }
+  }
+}
+`;
+export const onDeleteNote = `subscription OnDeleteNote {
+  onDeleteNote {
+    id
+    createdAt
+    longitude
+    latitude
+    content
+    user {
+      id
+      username
+      email
+      emailVerified
+      notes {
+        nextToken
+      }
+    }
+  }
+}
+`;

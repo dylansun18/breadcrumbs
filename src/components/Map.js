3735c20691cbbbebfb85759c35a6740386a04dfc
andrewlo1111 <andrewlo2528@gmail.com>
2019-03-30 20:33:22 -0700

    included a circle that will be displayed

---
+++ src/components/Map.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+import MapView, { Marker, Location, Permissions, Circle } from 'react-native-maps';
+
+export default class Map extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+	
+	state = {
+		region: null,
+		circle: null
+	};
+
+	componentWillMount() {
+		this.getLocationAsync();
+	}
+
+	getLocationAsync = async () => {
+		let { status } = await Permissions.askAsync(Permissions.LOCATION);
+		if (status !== 'granted') {
+			this.setState({
+			errorMessage: 'Permission to access location was denied'
+		});
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+	const current_region = {
+		latitude: location.coords.latitude,
+		longitude: location.coords.longitude,
+		latitudeDelta: previousState.region.latitudeDelta,
+		longitudeDelta: previousState.region.longitudeDelta,
+		};
+	const current_circle = {
+		latitude: location.coords.latitude,
+		longitude: location.coords.longitude
+		};
+		await this.setState({ region: current_region });
+		await this.setState({circle: current_circle});
+	}
+
+	static navigationOptions = {
+		header:null
+	};
+
+	componentDidMount() {
+		// Called once after the component is mounted
+	}
+
+	componentDidUpdate() {
+		// Called every time setState or forceUpdate is called
+	}
+
+	render() {
+		const { navigate } = this.props.navigation;
+		return (
+			<View>
+				<View>
+					<MapView
+						style={styles.container}
+						region={this.region}
+						showsUserLocation
+						showsMyLocationButton
+						followsUserLocation
+					>
+					<Circle
+						center = {this.state.circle}
+						radius = {1000}
+						/>
+				</MapView>
+				<View style={{top: 300}}>
+				<Button
+					onPress={() => navigate('Main')}
+					title="Return to Home Screen"
+					accessibilityLabel="Return to Home Screen"
+				/>
+				</View>
+			</View>
+		</View>
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    height: 300,
+	}
+});
